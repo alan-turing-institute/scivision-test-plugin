0008c471f2bc70b1703597bdb2bc2af341420fb3
quantumjot <code@arlowe.co.uk>
2021-07-09 13:21:10 +0100

    return type

--- scivision_test_plugin/model.py
+++ scivision_test_plugin/model.py
@@ -6,7 +6,7 @@ class DummyModel:
     def __init__(self):
         pass
 
-    def predict(self, data: np.ndarray, sigma: float = 3.0):
+    def predict(self, data: np.ndarray, sigma: float = 3.0) -> np.ndarray:
         return gaussian(data, sigma=sigma, preserve_range=True)
 
 
