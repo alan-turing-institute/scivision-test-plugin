0461058692632b16829fec7455d20d0e9afa3357
quantumjot <code@arlowe.co.uk>
2021-07-12 12:44:46 +0100

    small change to config/model

--- scivision_test_plugin/model.py
+++ scivision_test_plugin/model.py
@@ -6,8 +6,8 @@ class DummyModel:
     def __init__(self):
         pass
 
-    def predict(self, data: np.ndarray, sigma: float = 3.0) -> np.ndarray:
-        return gaussian(data, sigma=sigma, preserve_range=True)
+    def predict(self, image: np.ndarray, sigma: float = 3.0) -> np.ndarray:
+        return gaussian(image, sigma=sigma, preserve_range=True)
 
 
 if __name__ == "__main__":
