0fbd006c2c78390659e07fc58270a337a6ec9453
acocac <acocac@gmail.com>
2021-07-15 11:41:18 -0500

    add ImageNet

--- scivision_test_plugin/model.py
+++ scivision_test_plugin/model.py
@@ -1,5 +1,12 @@
+import os
+
+os.environ['TF_KERAS'] = '1'
+
 import numpy as np
 from skimage.filters import gaussian
+
+if os.environ.get('TF_KERAS'):
+    from classification_models.tfkeras import Classifiers
 
 
 class DummyModel:
@@ -15,12 +22,28 @@ class AnotherModel:
         pass
 
     def do_something(
-        self,
-        preserve_range: bool = True,
-        sigma: float = 10.0,
-        image: np.ndarray = np.empty((64, 64, 1)),
+            self,
+            preserve_range: bool = True,
+            sigma: float = 10.0,
+            image: np.ndarray = np.empty((64, 64, 1)),
     ) -> np.ndarray:
         return gaussian(image, sigma=sigma, preserve_range=preserve_range)
+
+
+class ImageNetModel:
+    def __init__(self):
+        self.pretrainedModel, self.preprocess_input = Classifiers.get('resnet18')
+
+    def predict(self, image: np.ndarray) -> np.ndarray:
+        model = self.pretrainedModel(
+            input_shape=(224, 224, 3), weights="imagenet", classes=1000
+        )
+
+        image = self.preprocess_input(image)
+        image = np.expand_dims(image, 0)
+
+        y = model.predict(image)
+        return y
 
 
 if __name__ == "__main__":
